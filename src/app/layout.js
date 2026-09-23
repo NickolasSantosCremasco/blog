a76d804e151d87b4e75f14f7bdde781c01379b1d
Nickolas Dos Santos Cremasco <nickolascremasco@gmail.com>
2026-09-23 16:59:53 -0300

    first commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,5 @@
+import Footer from './components/footer/Footer'
+import Navbar from './components/navbar/Navbar'
 import './globals.css'
 import { Inter } from 'next/font/google'
 
@@ -10,8 +12,16 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="pt-br">
+      <body className={inter.className}>
+        <div className='container'>
+          <div className='wrapper'>
+            <Navbar/>
+            {children}
+            <Footer/>
+          </div>
+        </div>
+      </body>
     </html>
   )
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,13 @@
+import Footer from "./components/footer/Footer";
+import Navbar from "./components/navbar/Navbar";
 import styles from "./homepage.module.css";
+import Link from "next/link";
 
 export default function Home() {
-  return <div>Hello</div>;
+  return (
+    <div>
+
+
+   </div>
+  );
 }
