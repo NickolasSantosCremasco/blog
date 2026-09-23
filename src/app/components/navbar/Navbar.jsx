d2dc84dd130eedfa8bb470fd69451d92c245aa2f
Nickolas Dos Santos Cremasco <nickolascremasco@gmail.com>
2026-09-23 17:51:36 -0300

    components

--- src/app/components/navbar/Navbar.jsx
+++ src/app/components/navbar/Navbar.jsx
@@ -2,7 +2,17 @@ import React from "react";
 import styles from "./navbar.module.css"
 const Navbar = () => {
     return (
-        <div className={styles.container}>Navbar</div>
+        <div className={styles.container}>
+            <div className={styles.social}>
+                <Image src="/facebook.png" alt="facebook" width={24} height={24}></Image>
+                <Image src="/instagram.png" alt="facebook" width={24} height={24}></Image>
+                <Image src="/tiktok.png" alt="facebook" width={24} height={24}></Image>
+                <Image src="/youtube.png" alt="facebook" width={24} height={24}></Image>
+            </div>
+            <div className={styles.logo}>Nickolas Blog</div>
+            <div className={styles.links}></div>
+            <div className={styles.links}></div>
+        </div>
     )
 }
 
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,13 +1,17 @@
-import Footer from "./components/footer/Footer";
-import Navbar from "./components/navbar/Navbar";
+
+import CategoryList from "./components/CategoryList/CategoryList";
+import Featured from "./components/featured/Feature";
 import styles from "./homepage.module.css";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-
-
+    <div className={styles.container}>
+      <Featured/>
+      <CategoryList/>
+      <div className={styles.content}>
+        <Card
+      </div>
    </div>
   );
 }
